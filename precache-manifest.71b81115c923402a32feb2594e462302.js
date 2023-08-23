c53ce57eb1319aae60dc8309bf61dbcf03b8abc0
szlaurel <szlaurel5@gmail.com>
2023-08-22 17:40:15 -0700

    Updates

---
+++ precache-manifest.71b81115c923402a32feb2594e462302.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d297b0d80b96401ade9",
+    "url": "/%7Bse_project_react/static/css/main.70c4af8c.chunk.css"
+  },
+  {
+    "revision": "9d297b0d80b96401ade9",
+    "url": "/%7Bse_project_react/static/js/main.9d297b0d.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/%7Bse_project_react/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "3ecd300016765f24cf4a",
+    "url": "/%7Bse_project_react/static/js/2.3ecd3000.chunk.js"
+  },
+  {
+    "revision": "3ec0cf714828b4cc9303",
+    "url": "/%7Bse_project_react/static/js/runtime~main.3ec0cf71.js"
+  },
+  {
+    "revision": "0b0a77ea669570cdc6b14ec58d5093a9",
+    "url": "/%7Bse_project_react/static/media/cloudy.0b0a77ea.svg"
+  },
+  {
+    "revision": "7c619ec4909a2e80216d7d99b6307fa0",
+    "url": "/%7Bse_project_react/static/media/Logo.7c619ec4.svg"
+  },
+  {
+    "revision": "bb10761e439814379f64ccf14e0744dd",
+    "url": "/%7Bse_project_react/static/media/Avatarlogo.bb10761e.svg"
+  },
+  {
+    "revision": "d162380bc5219380f5f4b10d8d618c97",
+    "url": "/%7Bse_project_react/static/media/sunny.d162380b.svg"
+  },
+  {
+    "revision": "4586d449b72fa2a36f69e80be9e46cef",
+    "url": "/%7Bse_project_react/static/media/cloud.4586d449.svg"
+  },
+  {
+    "revision": "20e0150eb0840e5f79c852d01c3dee21",
+    "url": "/%7Bse_project_react/static/media/moon.20e0150e.svg"
+  },
+  {
+    "revision": "26da5474ed75bbe22a529aefc19988f8",
+    "url": "/%7Bse_project_react/static/media/newclosebutton.26da5474.svg"
+  },
+  {
+    "revision": "fdbfc2270e5bd70f01a0092850b241ae",
+    "url": "/%7Bse_project_react/index.html"
+  }
+];
